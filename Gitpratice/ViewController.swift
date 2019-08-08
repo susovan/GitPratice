e7d7e5dc21037ca2a8e74084b910d2306abcda61
Susovan Pati <susovanpati@Susovans-MacBook-Air.local>
2019-08-08 11:20:41 +0530

    first commit

---
+++ Gitpratice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gitpratice
+//
+//  Created by Susovan Pati on 08/08/19.
+//  Copyright © 2019 Susovan Pati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
